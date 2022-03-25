17547c84c73ebb42e5118d55e7b2152ac0fc7db1
AZAZAZAZAZAZAZAladno <102374974+AZAZAZAZAZAZAZAladno@users.noreply.github.com>
2022-03-25 19:33:30 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,23 @@
 using namespace std;
 
 int main() {
+  
+  int a;
+  int b;
+  int c;
+  int d;
+  
+cout << "input a" << endl;
+cin >> a;
+  
+cout << "input b" << endl;
+cin >> b;
+  
+cout << "input с" << endl;  
+cin c;
 
-cout << "Hello, World" << endl;
-
+cout << "input d" << endl;
+cin d;
+  
 return 0;
 }
